1126ae0e006759cd2f041479f9ad3f752314e9af
Tinsae <iamtinsae.dev@gmail.com>
2023-04-02 10:05:56 +0300

    Refactor settings.py to use decouple for secret key and debug configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,19 +11,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_3ux49e165+0@%!a(x8k17@rgo7aros=i3yam10vhdu0%4czb("
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
